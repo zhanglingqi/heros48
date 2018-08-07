afde7291bbd891c2b2756fd0a2c1170049e8868a
zhanglingqi <504024956@qq.com>
2018-08-07 15:44:25 +0800

    数据渲染 实现添加按钮跳转

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,7 +4,8 @@ import Vue from 'vue';
 import HeroesList from '../views/heroes/List.vue';
 import EquipsList from '../views/equips/List.vue';
 import WeaponsList from '../views/weapons/List.vue';
-
+// 导入添加英雄的组件
+import HeroesAdd from '../views/heroes/Add.vue';
 // 注册VueRouter插件  this.$router  this.$route
 Vue.use(VueRouter);
 
@@ -13,8 +14,9 @@ var router = new VueRouter({
   linkExactActiveClass: 'active',
   // 路由规则
   routes: [
-	{ path: '/', redirect: '/heroes' },
+	  { path: '/', redirect: '/heroes' },
     { name: 'heroes', path: '/heroes', component: HeroesList },
+    { name: 'heroadd', path: '/heroes/add', component: HeroesAdd },
     { name: 'equips', path: '/equips', component: EquipsList },
     { name: 'weapons', path: '/weapons', component: WeaponsList }
   ]
