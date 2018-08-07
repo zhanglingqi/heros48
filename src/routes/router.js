91513fbee936feedf5ac1b42f6ecd15bc8b7c668
zhanglingqi <504024956@qq.com>
2018-08-07 11:52:34 +0800

    路由配置完成

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,14 +1,22 @@
 import VueRouter from 'vue-router';
 import Vue from 'vue';
+//导入路由
+import HeroesList from '../views/heroes/List.vue';
+import EquipsList from '../views/equips/List.vue';
+import WeaponsList from '../views/weapons/List.vue';
 
 // 注册VueRouter插件  this.$router  this.$route
 Vue.use(VueRouter);
 
 //创建路由对象
 var router = new VueRouter({
+  linkExactActiveClass: 'active',
   // 路由规则
   routes: [
-
+	{ path: '/', redirect: '/heroes' },
+    { name: 'heroes', path: '/heroes', component: HeroesList },
+    { name: 'equips', path: '/equips', component: EquipsList },
+    { name: 'weapons', path: '/weapons', component: WeaponsList }
   ]
 });
 
