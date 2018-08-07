a597dbc6a6c0fde0820378a4927205701539b4a4
zhanglingqi <504024956@qq.com>
2018-08-07 10:24:23 +0800

    首页的完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入全局样式
+// 导入bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+// 导入index.css
+import './assets/css/index.css';
+
 new Vue({
   el: '#app',
   render: h => h(App)
